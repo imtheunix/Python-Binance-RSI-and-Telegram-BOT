61aa945b8364839227611da3b27bdbf06b228a56
imtheunix <49956370+imtheunix@users.noreply.github.com>
2021-12-14 20:37:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -52,3 +52,14 @@ def vender():
         quantity=sell,
         price=price,
     )
+
+
+"""'
+Uncomment this if you want to send a telegram message
+def send_msg(text):
+    token = ""
+    chat_id = ""
+    url_req = "https://api.telegram.org/bot" + token + \
+        "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
+    results = requests.get(url_req)
+""" ""
